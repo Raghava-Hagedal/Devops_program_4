71302eeba0ec8f007c469273f84382eebcd9a1f8
Raghava-Hagedal <raghavahagedal@gmail.com>
2025-10-05 12:50:28 +0530

    Create program4.py

---
+++ program4.py
@@ -0,0 +1,6 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number"))
+sum = a + b
+print("The sum of", a, "and", b, "is", sum)
+sub = a - b
+print("The sum of", a, "and", b, "is",sub)

e6eabe1ac1a214e807e969c0fecaf1e4d4a05680
Raghava-Hagedal <raghavahagedal@gmail.com>
2025-10-12 16:46:27 +0530

    Update program4.py

--- program4.py
+++ program4.py
@@ -3,4 +3,8 @@ b=int(input("Enter second number"))
 sum = a + b
 print("The sum of", a, "and", b, "is", sum)
 sub = a - b
-print("The sum of", a, "and", b, "is",sub)
+print("The sub of", a, "and", b, "is",sub)
+mult = a * b
+print("The mult of", a, "and", b, "is",mult)
+div = a / b
+print("The div of", a, "and", b, "is",div)
